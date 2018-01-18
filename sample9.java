563acffe09a93a3b539e185a602e83f32e0e191f
Makoto Tanabe <xekid78@outlook.com>
2018-01-18 11:12:59 +0900

    標準入力

---
+++ sample9.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+import java.util.Scanner;
+
+public class sample9 {
+
+	public static void main(String[] args) {
+		System.out.println("文字の入力をお願いします。");
+		Scanner scan =new Scanner(System.in);
+		String data = scan.next();
+		System.out.println(data + "と入力しました。");
+
+	}
+
+}
