68c5d8288966665e1e7acf0a44d28fb5180347ac
Makoto Tanabe <xekid78@outlook.com>
2018-01-18 11:22:13 +0900

    Update sample9.java

--- sample9.java
+++ sample9.java
@@ -9,7 +9,7 @@ public class sample9 {
 
 	public static void main(String[] args) {
 		System.out.println("文字の入力をお願いします。");
-		Scanner scan =　new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		String data = scan.next();
 		System.out.println(data + "と入力しました。");
 
