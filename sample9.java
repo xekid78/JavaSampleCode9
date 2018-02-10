5a3dc3025d22cf225059bb02d03ff84d1022e0f0
Makoto Tanabe <xekid78@outlook.com>
2018-02-10 22:59:41 +0900

    try-with-resources文追加

--- sample9.java
+++ sample9.java
@@ -3,16 +3,21 @@
  * @author xekid78
  *
  */
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class sample9 {
 
 	public static void main(String[] args) {
 		System.out.println("文字の入力をお願いします。");
-		Scanner scan = new Scanner(System.in);
-		String data = scan.next();
-		System.out.println(data + "と入力しました。");
-		scan.close();
+		try (Scanner scan = new Scanner(System.in)) {
+			String data = scan.next();
+			System.out.println(data + "と入力しました。");
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
 
 	}
 
